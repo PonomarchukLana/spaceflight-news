e94ef7b2be81119e7243f1cfa71ed58318fbaeb0
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-01-12 17:12:54 +0200

    first commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-optional-chaining"
+    ],
+    presets: [
+        "@babel/preset-react",
+        "@babel/preset-typescript"
+    ]
+};
